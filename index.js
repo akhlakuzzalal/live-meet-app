7ef8b7251bce5db44a5dda0996557b8ab5f5b103
akhlakuzzalal <akhlakuzzalal@gmail.com>
2022-04-06 04:57:22 +0600

    try to solve

--- index.js
+++ index.js
@@ -11,13 +11,9 @@ const io = require('socket.io')(server, {
 });
 
 app.use(cors());
-app.use(express.static(path.join(__dirname, '/build')));
+app.use(express.static(path.join(__dirname, 'client', 'build')));
 
 const PORT = process.env.PORT || 5000;
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/build/index.html'));
-});
 
 io.on('connection', (socket) => {
   socket.emit('me', socket.id);
@@ -35,4 +31,7 @@ io.on('connection', (socket) => {
   });
 });
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+});
 server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
